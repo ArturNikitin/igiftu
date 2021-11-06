d8a83d69191ccdf00651327b76661b610a9926f1
Artur Nikitin <artur00794@gmail.com>
2021-11-06 12:36:55 +0300

    Added authorization to swagger

--- src/main/kotlin/com/svetka/igiftu/config/SwaggerConfig.kt
+++ src/main/kotlin/com/svetka/igiftu/config/SwaggerConfig.kt
@@ -4,18 +4,39 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiKey
+import springfox.documentation.service.AuthorizationScope
+import springfox.documentation.service.SecurityReference
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
+
 
 @Configuration
 class SwaggerConfig {
 
-    @Bean
-    fun swagger(): Docket {
-        return Docket(DocumentationType.OAS_30)
-            .select()
-            .apis(RequestHandlerSelectors.basePackage("com.svetka.igiftu.controller"))
-            .paths(PathSelectors.any())
-            .build()
-    }
+	@Bean
+	fun swagger(): Docket {
+		return Docket(DocumentationType.OAS_30)
+			.securityContexts(listOf(securityContext()))
+			.securitySchemes(listOf(apiKey()))
+			.select()
+			.apis(RequestHandlerSelectors.basePackage("com.svetka.igiftu.controller"))
+			.paths(PathSelectors.any())
+			.build()
+	}
+
+	private fun apiKey() = ApiKey("Authorization", "Authorization", "header")
+
+
+	private fun securityContext() =
+		SecurityContext.builder().securityReferences(defaultAuth()).build()
+
+
+	private fun defaultAuth(): List<SecurityReference> {
+		val authorizationScope = AuthorizationScope("global", "accessEverything")
+		val authorizationScopes: Array<AuthorizationScope?> = arrayOfNulls<AuthorizationScope>(1)
+		authorizationScopes[0] = authorizationScope
+		return listOf(SecurityReference("Authorization", authorizationScopes))
+	}
 }
