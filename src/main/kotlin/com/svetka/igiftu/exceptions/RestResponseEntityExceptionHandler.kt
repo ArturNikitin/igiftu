71bdb14cf51fe7ee53fcc2018198ef0301a1210b
Artur Nikitin <artur00794@gmail.com>
2021-11-25 20:46:18 +0300

    added json as response for errors

--- src/main/kotlin/com/svetka/igiftu/exceptions/RestResponseEntityExceptionHandler.kt
+++ src/main/kotlin/com/svetka/igiftu/exceptions/RestResponseEntityExceptionHandler.kt
@@ -1,12 +1,12 @@
 package com.svetka.igiftu.exceptions
 
-import java.util.HashMap
 import java.util.function.Consumer
 import javax.persistence.EntityExistsException
 import javax.persistence.EntityNotFoundException
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.util.LinkedMultiValueMap
 import org.springframework.validation.FieldError
 import org.springframework.validation.ObjectError
 import org.springframework.web.bind.MethodArgumentNotValidException
@@ -18,59 +18,54 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 class RestResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
 
-    override fun handleMethodArgumentNotValid(
-        ex: MethodArgumentNotValidException,
-        headers: HttpHeaders,
-        status: HttpStatus,
-        request: WebRequest
-    ): ResponseEntity<Any> {
-        val errors: MutableMap<String, String?> = HashMap()
-        ex.bindingResult.allErrors.forEach(Consumer { error: ObjectError ->
-            val fieldName = (error as FieldError).field
-            val errorMessage = error.getDefaultMessage()
-            errors["message"] = errorMessage
-        })
-        return handleExceptionInternal(
-            ex, errors, HttpHeaders.EMPTY,
-            HttpStatus.BAD_REQUEST, request
-        )
-    }
+	override fun handleMethodArgumentNotValid(
+		ex: MethodArgumentNotValidException,
+		headers: HttpHeaders,
+		status: HttpStatus,
+		request: WebRequest
+	): ResponseEntity<Any> {
+		val errors: MutableMap<String, String?> = HashMap()
+		ex.bindingResult.allErrors.forEach(Consumer { error: ObjectError ->
+//			val fieldName = (error as FieldError).field
+			val errorMessage = error.defaultMessage
+			errors["message"] = errorMessage
+		})
+		return handleExceptionInternal(
+			ex, errors, HttpHeaders.EMPTY,
+			HttpStatus.BAD_REQUEST, request
+		)
+	}
 
-    @ExceptionHandler(
-        value = [EntityNotFoundException::class]
-    )
-    fun handleEntityNotFound(
-        ex: EntityNotFoundException,
-        request: WebRequest
-    ): ResponseEntity<Any> = handleExceptionInternal(
-        ex, ex.message,
-        HttpHeaders.EMPTY, HttpStatus.NOT_FOUND, request
-    )
+	@ExceptionHandler(value = [EntityNotFoundException::class])
+	fun handleEntityNotFound(ex: EntityNotFoundException, request: WebRequest) = prepareResponse(
+		ex, mapOf("message" to (ex.message ?: "")),
+		HttpStatus.NOT_FOUND, request
+	)
 
-    @ExceptionHandler(
-        value = [EntityExistsException::class]
-    )
-    fun handleEntityExists(
-        ex: EntityExistsException,
-        request: WebRequest
-    ): ResponseEntity<Any> {
-        val errors: MutableMap<String, String?> = HashMap()
-        errors["message"] = ex.message
-        return handleExceptionInternal(
-            ex, errors,
-            HttpHeaders.EMPTY, HttpStatus.NOT_FOUND, request
-        )
-    }
+	@ExceptionHandler(
+		value = [EntityExistsException::class]
+	)
+	fun handleEntityExists(ex: EntityExistsException, request: WebRequest) = prepareResponse(
+		ex, mapOf("message" to (ex.message ?: "")),
+		HttpStatus.NOT_FOUND, request
+	)
 
-    @ExceptionHandler(
-            value = [SecurityException::class]
-    )
-    fun handleEntityExists(
-        ex: SecurityException,
-        request: WebRequest
-    ): ResponseEntity<Any> = handleExceptionInternal(
-            ex, ex.message,
-            HttpHeaders.EMPTY, HttpStatus.UNAUTHORIZED, request
-    )
+	@ExceptionHandler(value = [SecurityException::class])
+	fun handleSecurityException(ex: SecurityException, request: WebRequest) = prepareResponse(
+		ex, mapOf("message" to (ex.message ?: "")),
+		HttpStatus.UNAUTHORIZED, request
+	)
+
+	private fun prepareResponse(
+		ex: Exception,
+		json: Map<String, String>,
+		status: HttpStatus,
+		request: WebRequest
+	) = handleExceptionInternal(
+		ex, json, HttpHeaders(
+			LinkedMultiValueMap<String, String>()
+				.apply { add("Content-Type", "application/json") }),
+		status, request
+	)
 
 }
