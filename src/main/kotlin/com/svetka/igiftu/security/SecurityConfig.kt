3b88037b31491f4453025334125005d3a5a69229
ArturNikitin <Artur00794@gmail.com>
2021-12-15 17:36:38 +0300

    feat: added endpoint auth/google for OAuth2

--- src/main/kotlin/com/svetka/igiftu/security/SecurityConfig.kt
+++ src/main/kotlin/com/svetka/igiftu/security/SecurityConfig.kt
@@ -1,9 +1,9 @@
 package com.svetka.igiftu.security
 
-import com.svetka.igiftu.security.jwt.SecurityExceptionHandlerFilter
 import com.svetka.igiftu.security.jwt.JwtConfig
 import com.svetka.igiftu.security.jwt.JwtTokenVerifier
 import com.svetka.igiftu.security.jwt.JwtUsernameAndPasswordAuthenticationFilter
+import com.svetka.igiftu.security.jwt.SecurityExceptionHandlerFilter
 import com.svetka.igiftu.security.service.facebook.FacebookSignInAdapter
 import java.security.SecureRandom
 import javax.crypto.SecretKey
@@ -20,6 +20,7 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.access.AccessDeniedHandler
 import org.springframework.social.connect.ConnectionFactoryLocator
@@ -37,94 +38,98 @@ import org.springframework.web.servlet.HandlerExceptionResolver
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 class SecurityConfig(
-    private val userDetailsService: UserDetailsService,
-    private val secretKey: SecretKey,
-    private val jwtConfig: JwtConfig,
-    private val facebookSignUp: ConnectionSignUp,
-    @Qualifier("handlerExceptionResolver")
-    private val resolver: HandlerExceptionResolver,
-    @Qualifier("customAccessDeniedHandler")
-    private val accessDeniedHandler: AccessDeniedHandler,
-    @Qualifier("customAuthenticationEntryPoint")
-    private val entryPoint: AuthenticationEntryPoint
+	private val userDetailsService: UserDetailsService,
+	private val secretKey: SecretKey,
+	private val jwtConfig: JwtConfig,
+	private val facebookSignUp: ConnectionSignUp,
+	@Qualifier("handlerExceptionResolver")
+	private val resolver: HandlerExceptionResolver,
+	@Qualifier("customAccessDeniedHandler")
+	private val accessDeniedHandler: AccessDeniedHandler,
+	@Qualifier("customAuthenticationEntryPoint")
+	private val entryPoint: AuthenticationEntryPoint
 ) : WebSecurityConfigurerAdapter() {
 
-    @Value("\${spring.social.facebook.appId}")
-    private lateinit var appId: String
+	@Value("\${spring.social.facebook.appId}")
+	private lateinit var appId: String
 
-    @Value("\${spring.social.facebook.appSecret}")
-    private lateinit var appSecret: String
+	@Value("\${spring.social.facebook.appSecret}")
+	private lateinit var appSecret: String
 
-    override fun configure(auth: AuthenticationManagerBuilder?) {
-        auth?.userDetailsService(userDetailsService)
-    }
+	override fun configure(auth: AuthenticationManagerBuilder?) {
+		auth?.userDetailsService(userDetailsService)
+	}
 
-    override fun configure(http: HttpSecurity) {
-        http.cors().configurationSource {
-            CorsConfiguration().apply {
-                allowedOrigins = listOf("http://localhost:3000")
-                allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
-                allowedHeaders = listOf("*")
-            }
-        }
-            .and()
-            .csrf().disable()
-            .exceptionHandling()
-            .accessDeniedHandler(accessDeniedHandler)
-            .authenticationEntryPoint(entryPoint)
-            .and()
-            .addFilterBefore(
-                SecurityExceptionHandlerFilter(resolver),
-                JwtUsernameAndPasswordAuthenticationFilter::class.java
-            )
-            .addFilter(
-                jwtUsernameAndPasswordAuthenticationFilter()
-            )
-            .addFilterAfter(
-                JwtTokenVerifier(jwtConfig, secretKey),
-                JwtUsernameAndPasswordAuthenticationFilter::class.java
-            )
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and().authorizeRequests()
-            .antMatchers("/swagger-ui/**").permitAll()
-            .antMatchers("/swagger-resources/**").permitAll()
-            .antMatchers("/v2/api-docs**").permitAll()
-            .antMatchers("/login").permitAll()
-            .antMatchers("/user/login").authenticated()
-            .antMatchers("/registration").permitAll()
-            .antMatchers("/user/**").permitAll()
-            .antMatchers("/signin/**").permitAll()
-            .antMatchers("/api/**").hasRole("ADMIN")
-    }
+	override fun configure(http: HttpSecurity) {
+		http.cors().configurationSource {
+			CorsConfiguration().apply {
+				allowedOrigins = listOf("http://localhost:3000")
+				allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+				allowedHeaders = listOf("*")
+			}
+		}
+			.and()
+			.csrf().disable()
+			.exceptionHandling()
+			.accessDeniedHandler(accessDeniedHandler)
+			.authenticationEntryPoint(entryPoint)
+			.and()
+			.addFilterBefore(
+				SecurityExceptionHandlerFilter(resolver),
+				JwtUsernameAndPasswordAuthenticationFilter::class.java
+			)
+			.addFilterAfter(
+				jwtUsernameAndPasswordAuthenticationFilter(),
+				OAuth2LoginAuthenticationFilter::class.java
+			)
+			.addFilterAfter(
+				JwtTokenVerifier(jwtConfig, secretKey),
+				JwtUsernameAndPasswordAuthenticationFilter::class.java
+			)
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and().authorizeRequests()
+			.antMatchers("/swagger-ui/**").permitAll()
+			.antMatchers("/swagger-resources/**").permitAll()
+//            .antMatchers("/login/**").permitAll()
+			.antMatchers("/user/login").authenticated()
+			.antMatchers("/registration").permitAll()
+			.antMatchers("/user/**").permitAll()
+			.mvcMatchers("/auth/**").permitAll()
+			.antMatchers("/api/**").hasRole("ADMIN")
+			.anyRequest().authenticated().and().oauth2Login()
+			.authorizationEndpoint().baseUri("/auth")
+	}
 
-    private fun jwtUsernameAndPasswordAuthenticationFilter() =
-        JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), secretKey, jwtConfig)
-            .apply { this.setFilterProcessesUrl("/user/login") }
+	private fun jwtUsernameAndPasswordAuthenticationFilter() =
+		JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), secretKey, jwtConfig)
+			.apply {
+				this.setFilterProcessesUrl("/user/login")
+			}
 
-    @Bean
-    fun passwordEncoderBean(): PasswordEncoder {
-        return BCryptPasswordEncoder(8, SecureRandom())
-    }
+	@Bean
+	fun passwordEncoderBean(): PasswordEncoder {
+		return BCryptPasswordEncoder(8, SecureRandom())
+	}
 
-    @Bean
-    fun providerSignInController(): ProviderSignInController {
-        val connectionFactoryLocator = connectionFactoryLocator()
-        val usersConnectionRepository = getUsersConnectionRepository(connectionFactoryLocator)
-        (usersConnectionRepository as InMemoryUsersConnectionRepository)
-            .setConnectionSignUp(facebookSignUp)
-        return ProviderSignInController(
-            connectionFactoryLocator,
-            usersConnectionRepository, FacebookSignInAdapter()
-        )
-    }
+	@Bean
+	fun providerSignInController(): ProviderSignInController {
+		val connectionFactoryLocator = connectionFactoryLocator()
+		val usersConnectionRepository = getUsersConnectionRepository(connectionFactoryLocator)
+		(usersConnectionRepository as InMemoryUsersConnectionRepository)
+			.setConnectionSignUp(facebookSignUp)
+		return ProviderSignInController(
+			connectionFactoryLocator,
+			usersConnectionRepository, FacebookSignInAdapter()
+		)
+	}
 
-    private fun connectionFactoryLocator(): ConnectionFactoryLocator {
-        val registry = ConnectionFactoryRegistry()
-        registry.addConnectionFactory(FacebookConnectionFactory(appId, appSecret))
-        return registry
-    }
+	private fun connectionFactoryLocator(): ConnectionFactoryLocator {
+		val registry = ConnectionFactoryRegistry()
+		registry.addConnectionFactory(FacebookConnectionFactory(appId, appSecret))
+		return registry
+	}
 
-    private fun getUsersConnectionRepository(connectionFactoryLocator: ConnectionFactoryLocator): UsersConnectionRepository {
-        return InMemoryUsersConnectionRepository(connectionFactoryLocator)
-    }
+	private fun getUsersConnectionRepository(connectionFactoryLocator: ConnectionFactoryLocator): UsersConnectionRepository {
+		return InMemoryUsersConnectionRepository(connectionFactoryLocator)
+	}
 }
