fa7bcd3e75a3fe095804189a8d55a66d983c2dc1
ArturNikitin <Artur00794@gmail.com>
2021-12-17 18:24:35 +0300

    fix: added backend redirect url

--- src/main/kotlin/com/svetka/igiftu/security/oauth/CustomSimpleUrlAuthenticationSuccessHandler.kt
+++ src/main/kotlin/com/svetka/igiftu/security/oauth/CustomSimpleUrlAuthenticationSuccessHandler.kt
@@ -2,17 +2,22 @@ package com.svetka.igiftu.security.oauth
 
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
 import org.springframework.stereotype.Component
 
 @Component
 class CustomSimpleUrlAuthenticationSuccessHandler: SimpleUrlAuthenticationSuccessHandler() {
+
+	@Value("\${redirect.backend}")
+	private lateinit var signUpUrl: String
+
 	override fun onAuthenticationSuccess(
 		request: HttpServletRequest,
 		response: HttpServletResponse,
 		authentication: Authentication?
 	) {
-		redirectStrategy.sendRedirect(request, response, "http://localhost:8081/signUp")
+		redirectStrategy.sendRedirect(request, response, signUpUrl)
 	}
 }
